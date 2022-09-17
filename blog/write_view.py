e035edf30d4594c5c22ea07812cd1b5af67604c5
comet <choihynmin1822@gmail.com>
2022-09-18 01:17:18 +0900

    Developing

--- blog/write_view.py
+++ blog/write_view.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
+from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.decorators import method_decorator
 
+@method_decorator(staff_member_required(login_url='account:signin'), name='dispatch')
 class WriteView(TemplateView):
     template_name = "blog/write.html"
 
