efa0c6f9559a21561150ac97254547ec9ae11c0f
comet <choihynmin1822@gmail.com>
2022-09-19 21:37:12 +0900

    Developing

--- blog/write_view.py
+++ blog/write_view.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import TemplateView
+from django.views.generic import FormView
 from django.contrib.admin.views.decorators import staff_member_required
 from django.utils.decorators import method_decorator
 
 @method_decorator(staff_member_required(login_url='account:signin'), name='dispatch')
-class WriteView(TemplateView):
+class WriteView(FormView):
     template_name = "blog/write.html"
-
-    def get(self, request, *args, **kwargs):
-        context = {}
-        return self.render_to_response(context)
